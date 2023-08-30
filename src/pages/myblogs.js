6bcec42e4081ca581069406aa28537fe3ca3eea6
Akiko Yamasaki <paonpaon@gmail.com>
2023-08-30 22:24:53 +1000

    first commit

---
+++ src/pages/myblogs.js
@@ -0,0 +1,13 @@
+import * as React from "react";
+import Layout from "../components/layout";
+
+const BlogPage = () => {
+  return (
+    <Layout pageTitle="Marisa Howard">
+      <p>contents</p>
+    </Layout>
+  );
+};
+
+export const Head = () => <title>Blog</title>;
+export default BlogPage;
