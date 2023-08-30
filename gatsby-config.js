c9fb684f73d0ec83d6f12a74cb17d80ae067668b
Akiko Yamasaki <paonpaon@gmail.com>
2023-08-30 12:05:41 +1000

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `rishowardsite`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
